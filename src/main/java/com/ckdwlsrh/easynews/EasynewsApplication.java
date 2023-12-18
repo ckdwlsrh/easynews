a132cc3ac7ca3bee9ae9ff59661ae2c2963e7eb6
ckdwlsrh <ckdwls6504@gmail.com>
2023-12-18 10:05:20 +0900

    init

---
+++ src/main/java/com/ckdwlsrh/easynews/EasynewsApplication.java
@@ -0,0 +1,13 @@
+package com.ckdwlsrh.easynews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasynewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasynewsApplication.class, args);
+	}
+
+}
